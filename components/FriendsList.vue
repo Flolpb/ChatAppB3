4180dbd4bfaff0e2f9466c2780694c8903fa0715
Raphaldeouf <raphaelvannier2001@gmail.com>
2021-12-16 11:04:47 +0100

    Ajout Barre de recherche affichant tout les utilisateurs sans doublons

--- components/FriendsList.vue
+++ components/FriendsList.vue
@@ -1,14 +1,14 @@
-<template>
+<template> 
     <div>
-        <div>
-        <img style="margin-left: 4px; margin-right: 4px; margin-top: 4px;border-radius: 50%; height: 2rem;" :src="photoUrl" />
-        </div>
-      <div>
-        <div>
+      <v-toolbar>
+        <v-toolbar-avatar>
+        <img style="margin-left: 4px; margin-right: 4px; margin-top: 4px;border-radius: 50%; height: 2rem;" :src="photoURL" />
+        </v-toolbar-avatar>
+        <v-toolbar-title>
           <div>{{username}}</div>
           <div style="width: 2rem;"></div>
-        </div>
-      </div>
+        </v-toolbar-title>
+      </v-toolbar>
     </div>
 
 </template>
@@ -16,37 +16,10 @@
 <script>
 
 export default ({
-    data: () => ({
-        username: null,
-        //state: null,
-        photoUrl: null,
-    }),
     props: [
-        "uid",
-        "fid",
+        "username",
+        "photoURL",
     ],
-    metaInfo () {
-      return {
-        id: this.id,
-        uid: this.uid,
-        fid: this.fid,
-      }
-    },
-    methods: {
-      async getData(){
-        const userRef = await this.$fire.firestore.collection("users").doc(this.fid);
-        const snapshot = await userRef.get();
-        const doc = snapshot.data();
-        if(doc != null){
-          this.username = doc.displayName;
-          this.photoUrl = doc.photoURL;
-          }
-        }
-         
-    },
-    async mounted () {
-      await this.getData();
-    },
 })
 </script>
 
--- pages/friends.vue
+++ pages/friends.vue
@@ -6,94 +6,157 @@
       >
         mdi-chevron-left
       </v-icon>
-    <v-toolbar
-      color="black"
-      width="400"
-      dense
-    >
-      <v-text-field
-        append-icon="mdi-magnify"
-        label="Rechercher..."
-        single-line
-        color="white"
-        hide-details
-      ></v-text-field>
-    </v-toolbar>
+      <v-form>
+        <v-flex xs6>
+          <v-autocomplete
+            v-model="searchString"
+            :disabled="isUpdating"
+            :items="search"
+            solo
+            chips
+            label="Rechercher..."
+            item-text="displayName"
+            item-value="displayName"
+            multiple
+            clearable
+            >
+            <template slot="no-data">
+              <v-list-tile>
+                <v-list-tile-title>
+                  ...Chercher de nouveaux amis
+                </v-list-tile-title>
+              </v-list-tile>
+            </template>
+             <template>         
+            <div v-for="(s, i) in search"
+              :key="i"
+              exact>
+              <SearchBar :username="s.displayName" :photoURL="s.photoURL"/>
+            </div>
+            </template>
+          </v-autocomplete>
+        </v-flex>
+      </v-form>
     <div>
-    <!--<p v-if="$fetchState.pending">Fetching Friends...</p>
-    <p v-else-if="$fetchState.error">An error occurred :(</p>-->
-      <ul>
-        <li v-for="(f, i) in friends"
-          :key="i"
-          exact>
-            <FriendsList :uid="f.userId" :fid="f.friendId"/>
-          </li>
-        </ul>
-      </div>
+      <v-card
+        elevation="12"
+        width="1000"
+      >
+        <v-navigation-drawer
+          color="deep-purple lighten-5"
+          width="1000"
+          floating
+          permanent
+        >
+          <v-list
+            dense
+            rounded
+          >
+            <v-list-item v-for="(f, i) in friends"
+              :key="i"
+              exact>
+                <FriendsList :username="f.displayName" :photoURL="f.photoURL"/>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
     </div>
+  </div>
 </template>
 
 <script>
   export default {
     data: () => ({
-      friend: {
-        id: "",
-        friendId: "",
-        userId: "",
-      },
       friends: [],
-      friendRef: null,
+      search: [],
     }),
     methods: {
       home () {
         this.$router.push("/planets");
       },
+      async getSearch(){
+        console.log("debut getSearch")
+        const usersRef = await this.$fire.firestore.collection("users");
+        const snap = await usersRef.get();
+        snap.forEach(doc => {
+          const user = doc.data();
+          if(user != null){
+            let i = this.search.length;
+              if(i != 0){
+                const id = this.search.find((s) => {
+                  return s.id == user.id;
+                });
+                if(id != undefined){
+                  this.search.push(user);
+                  i++;
+                }
+              }else{
+                  this.search.push(user);
+                  i++;
+              }
+          }
+        });
+      },
+
       async getFriends(){
-        const friendRef = await this.$fire.firestore.collection("usersFriends").doc(this.$store.state.auth.user.id);
-        const observer = friendRef.onSnapshot((doc) => {
-          const data = doc.data();
+        console.log("debut getFriends")
+        const userRef = await this.$fire.firestore.collection("users").doc(this.$store.state.auth.user.uid);
+        const snap = await userRef.get();
+        const data = snap.data();
           if(data != null){
             let i = this.friends.length;
+            if (data.friends != undefined){
             data.friends.forEach((fr) => {
-              if(i != 0){
+              if (i != 0){
                 const id = this.friends.find((f) => {
                   return f.id == fr.id;
                 });
-                if(id == undefined){
+                if(id != undefined){
                   this.friends.push(fr);
                   i++;
                 }
                 }else{
-                  this.friends.push(fr);
+                  this.friends.push(user);
                   i++;
-                }
-              })                
+              }
+            })                
+          }        
+          }
+      },
+      async addFriends(){
+            this.spinner = true;
+            try {
+              
+            } catch (e) {
+             this.spinner = false;
             }
-          })        
-        },
-
-        async mounted() {
-          this.friend.userId = this.$store.state.auth.user.id;
-          await this.getFriends();
-
-
-        },
-        async fetch() {
-          await this.getFriends();
-        },
+          },
     },
+        async mounted() {
+          await this.getFriends();
+          await this.getSearch();
+        }
+    
   }
 </script>
 
 <style scoped>
-    .v-card,.v-navigation-drawer{
+    .v-card,.v-toolbar{
         margin-left: auto;
         margin-right: auto;
         margin-top: 5%;
 }
     .colonne{
       column-count: 1;
-      width: auto;
-      margin-top: 3%;
     }
+
+    .v-form{
+    margin-left: 33%;
+    margin-right: auto;
+    margin-top: 5%;
+}
+
+    .v-select.v-select--is-menu-active .v-input__icon--append .v-icon {
+    transform: none;
+}
 </style>
